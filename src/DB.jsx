f5dea3da7d9d14a69b4e7a25550658f7161421f0
unknown <marianodaruich0@gmail.com>
2023-10-15 20:58:28 -0300

    Segundo Commit

--- src/DB.jsx
+++ src/DB.jsx
@@ -1,4 +1,35 @@
-import { createPool } from 'mysql2'; 
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+const db = mysql.createConnection({
+  host:"localhost",
+  user: "root",
+password: "mardar346",
+database: "FinalProg3"
+})
+
+app.post('/login', (req, res) => {
+  const sql ="SELECT * FROM login WHERE username = ? AND password = ?";
+  const values = [
+    req.body.email,
+    req.body.password
+  ]
+  db.query(sql, [values], (err, data) => {
+    if(err) return res.json("Login Failed");
+    return res.json(data);
+  })
+})
+app.listen(3000, () => {
+  console.log('Escuchando...')
+})
+
+/*import { createPool } from 'mysql2'; 
 const express = require('express');
 const app = express();
 const mysql = require('mysql2');
@@ -27,4 +58,4 @@ app.post('/register', (req, res) => {
       }
     }
   );
-});
+});*/
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-//import axios from 'axios';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 
@@ -7,25 +7,30 @@ export default function Login () {
 function Login() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-
+  function handleSubmit(event) {
+    event.preventDefault();
+    axios.post('')
+  }
+}
   return (
     <div className='d-flex vh-100 justify-content-center align-items-center bg-primary'>
       <div className='p-3 bg-white w-25'>
-        <form action="">
+        <form onSubmit={handleSubmit}>
         <div className='mb-3'>
           <label htmlFor="email">Email</label>
-          <input type="email" placeholder='Enter Email' className='form-control' />
+          <input type="email" placeholder='Enter Email' className='form-control'
+          onChange={e => setEmail(e.target.value)} />
           </div>
           <div className='mb-3'>
             <label htmlFor="password">Contraseña</label>
-          <input type="password" placeholder='Enter Password' className='form-control'/>
+          <input type="password" placeholder='Enter Password' className='form-control' 
+                    onChange={e => setPassword(e.target.value)} />
           </div>
           <button className='btn btn-success'>Login</button>
         </form>
       </div>
     </div>
   )
-}
 }
 /*export default function Login() {
   const loguear = async () => {
